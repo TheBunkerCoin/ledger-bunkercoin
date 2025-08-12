6997bdf7308e17e3add7139232c8e5f610ced72a
BunkerCoin <contact@bunkercoin.io>
2025-08-12 23:12:41 +0200

    fix: ledger compliance (9/x)

--- src/ui/display.h
+++ src/ui/display.h
@@ -3,11 +3,15 @@
 #include <stdbool.h>  // bool
 
 #if defined(TARGET_NANOX) || defined(TARGET_NANOS2)
-#define ICON_APP_BOILERPLATE C_app_boilerplate_14px
-#define ICON_APP_HOME        C_home_boilerplate_14px
+#define ICON_APP_BOILERPLATE C_app_bunkercoin_14px
+#define ICON_APP_HOME        ICON_APP_BOILERPLATE
 #define ICON_APP_WARNING     C_icon_warning
-#elif defined(TARGET_STAX) || defined(TARGET_FLEX)
-#define ICON_APP_BOILERPLATE C_app_boilerplate_64px
+#elif defined(TARGET_STAX)
+#define ICON_APP_BOILERPLATE C_app_bunkercoin_32px
+#define ICON_APP_HOME        ICON_APP_BOILERPLATE
+#define ICON_APP_WARNING     C_Warning_64px
+#elif defined(TARGET_FLEX)
+#define ICON_APP_BOILERPLATE C_app_bunkercoin_40px
 #define ICON_APP_HOME        ICON_APP_BOILERPLATE
 #define ICON_APP_WARNING     C_Warning_64px
 #endif
